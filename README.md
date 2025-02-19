56a82924c9c7a5ed93d514224f1df5fe89da414c
Alex <smokevsop@gmail.com>
2025-02-19 07:18:05 +0200

    README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Description
+'FOSSEn' is a free and open source search engine written in C/C++. The basic idea behind ​​this project is to implement an alternative client for the 'YaCy' network.
+
+# Requirements
+To build it you will need to install some development packages:
+
+CURL
+`apt install libcurl4-openssl-dev`
+
+SpiderMonkey
+`apt install libmozjs-91-dev`
+
+Jansson
+`apt install libjansson-dev`
+
+ZLib
+`apt install zlib1g-dev`
--- main.cpp
+++ main.cpp
@@ -1,1 +1,43 @@
 #include "main.hpp"
+
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp)
+{
+	size_t totalSize = size * nmemb;
+	userp->append((char*)contents, totalSize);
+	return totalSize;
+}
+
+std::string fetchPageContent(const std::string& url)
+{
+	CURL* curl;
+	CURLcode res;
+	std::string readBuffer;
+
+	curl = curl_easy_init();
+	if (curl)
+	{
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+
+		if (res != CURLE_OK)
+		{
+			std::cerr << "cURL error: " << curl_easy_strerror(res) << std::endl;
+		}
+	}
+	return readBuffer;
+}
+
+int main()
+{
+	std::string pg=fetchPageContent("https://www.thingiverse.com");
+	std::cout<<pg;
+
+	return(0);
+}
