7df35d461a21cbf594bf2a9dfcd50394b41e474c
Alex <smokevsop@gmail.com>
2025-07-28 09:58:42 +0200

    hash functions

--- simple_hash_func.cpp
+++ simple_hash_func.cpp
@@ -1,57 +1,80 @@
 #include "simple_hash_func.hpp"
 
-static const uint32_t MMSHASH_ALPHA_32=0xBC948A27;
-static const uint32_t MMSHASH_START_VALUE_32=0xDEADBEEF;
+static const uint32_t MMS32_ALPHA=0x7F545415;
+static const uint64_t MMS64_ALPHA=0x000007F22254544B;
 
-static const uint64_t MMSHASH_ALPHA_64=0xA58B6A35BC948A27;
-static const uint64_t MMSHASH_START_VALUE_64=0xDEADBEEFDEADBEEF;
+static const uint32_t FNV32_INITIAL_OFFSET=0x811C9DC5;
+static const uint64_t FNV64_INITIAL_OFFSET=0xCBF29CE484222325;
+
+static const uint32_t FNV32_PRIME=0x01000193;
+static const uint64_t FNV64_PRIME=0x00000100000001B3;
 
 uint32_t mms_hash_32(const uint8_t *data, uint32_t len)
 {
-	uint32_t mms_result=MMSHASH_START_VALUE_32, i;
-	for (i=0; i < len; i++)
+	uint32_t result=FNV32_INITIAL_OFFSET, i;
+	for(i=0; i<len; i++)
 	{
-		mms_result+=data[i];
-		mms_result=(mms_result & UINT16_MAX) * MMSHASH_ALPHA_32 + (mms_result >> 16);
+		result+=data[i];
+		result=(result & UINT16_MAX) * MMS32_ALPHA + (result >> 16);
 	}
-	return (mms_result);
+	return(result);
 }
 
 uint64_t mms_hash_64(const uint8_t *data, uint64_t len)
 {
-	uint64_t mms_result=MMSHASH_START_VALUE_64, i;
-	for (i=0; i < len; i++)
+	uint64_t result=FNV64_INITIAL_OFFSET, i;
+	for(i=0; i<len; i++)
 	{
-		mms_result+=data[i];
-		mms_result=(mms_result & UINT32_MAX) * MMSHASH_ALPHA_64 + (mms_result >> 32);
+		result+=data[i];
+		result=(result & UINT32_MAX) * MMS64_ALPHA + (result >> 32);
 	}
-	return (mms_result);
+	return(result);
 }
-
-static const uint32_t FNV32_OFFSET =0x811C9DC5;
-static const uint32_t FNV32_PRIME  =0x01000193;
-
-static const uint64_t FNV64_OFFSET =0xCBF29CE484222325;
-static const uint64_t FNV64_PRIME  =0x00000100000001B3;
 
 uint32_t fnv1a_hash_32(const uint8_t *inbuf, uint32_t inbuf_len)
 {
-	uint32_t fnv_result=FNV32_OFFSET, i;
-	for (i=0; i < inbuf_len; i++)
+	uint32_t result=FNV32_INITIAL_OFFSET, i;
+	for(i=0; i<inbuf_len; i++)
 	{
-		fnv_result^=inbuf[i];
-		fnv_result*=FNV32_PRIME;
+		result^=inbuf[i];
+		result*=FNV32_PRIME;
 	}
-	return (fnv_result);
+	return(result);
 }
 
 uint64_t fnv1a_hash_64(const uint8_t *inbuf, uint64_t inbuf_len)
 {
-	uint64_t fnv_result=FNV64_OFFSET, i;
-	for (i=0; i < inbuf_len; i++)
+	uint64_t result=FNV64_INITIAL_OFFSET, i;
+	for(i=0; i<inbuf_len; i++)
 	{
-		fnv_result^=inbuf[i];
-		fnv_result*=FNV64_PRIME;
+		result^=inbuf[i];
+		result*=FNV64_PRIME;
 	}
-	return (fnv_result);
+	return(result);
+}
+
+uint32_t xorshift_hash_32(const uint8_t *data, uint32_t len)
+{
+	uint32_t result=FNV32_INITIAL_OFFSET, i;
+	for(i=0; i<len; i++)
+	{
+		result+=data[i];
+		result^=result<<13;
+		result^=result>>17;
+		result^=result<<5;
+	}
+	return(result);
+}
+
+uint64_t xorshift_hash_64(const uint8_t *data, uint64_t len)
+{
+	uint64_t result=FNV64_INITIAL_OFFSET, i;
+	for(i=0; i<len; i++)
+	{
+		result+=data[i];
+		result^=result<<13;
+		result^=result>>7;
+		result^=result<<17;
+	}
+	return(result);
 }
--- simple_hash_func.hpp
+++ simple_hash_func.hpp
@@ -9,4 +9,7 @@ uint64_t mms_hash_64(const uint8_t *data, uint64_t len);
 uint32_t fnv1a_hash_32(const uint8_t *inbuf, uint32_t inbuf_len);
 uint64_t fnv1a_hash_64(const uint8_t *inbuf, uint64_t inbuf_len);
 
+uint32_t xorshift_hash_32(const uint8_t *data, uint32_t len);
+uint64_t xorshift_hash_64(const uint8_t *data, uint64_t len);
+
 #endif // SIMPLE_HASH_FUNC_H
